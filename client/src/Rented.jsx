6e066f201d87ca0f67f93b6f7e234d08f37753f1
Justine Chu <justinegeechu333@gmail.com>
2023-12-20 01:04:03 -0600

    rented page design update

--- client/src/Rented.jsx
+++ client/src/Rented.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Button } from "semantic-ui-react";
-import * as confetti from "confettis";
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import * as confetti from 'confettis';
 
 const Rented = () => {
     const { confirmationId } = useParams();
@@ -10,17 +10,20 @@ const Rented = () => {
         confetti.create();
     }, []);
     return (
-        <div>
-            <h2>Rented Successfully!</h2>
-            <h3>Confirmation Id: {confirmationId}</h3>
-            <Button
-                data-test="test"
-                onClick={() => {
-                    navigate("/");
-                }}
-            >
-                Go back Home
-            </Button>
+        <div className='w-screen h-[calc(100vh-10rem)] flex items-center justify-center backdrop-brightness-50 -mx-4'>
+            <div className='w-[300px] h-[200px] shadow-lg rounded-md border-neutral-700 border bg-white flex flex-col p-4 relative gap-4'>
+                <h2 className='text-2xl'>Rented Successfully!</h2>
+                <h3>Confirmation Id: {confirmationId}</h3>
+                <Button
+                    data-test='test'
+                    onClick={() => {
+                        navigate('/');
+                    }}
+                    className='absolute bottom-4 right-4'
+                >
+                    Go back Home
+                </Button>
+            </div>
         </div>
     );
 };
